b7617b40b55af73c95ff54eceb0ea70087b84ad0
aries0d0f <aries0d0f@gmail.com>
2023-04-04 14:07:18 +0800

    build: Configure PWA plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+
+import Vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    Vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        navigateFallback: 'index.html',
+        navigateFallbackDenylist: [/.*\.(jpg|png|svg|json|js)$/]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
