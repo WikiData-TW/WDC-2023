c7bd414f42b4972590fb980ed1c5f8ddd8bdaaa0
aries0d0f <aries0d0f@gmail.com>
2023-04-04 16:58:36 +0800

    feat(view): Implement MainBanner.

---
+++ src/components/Layout/MainBanner.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useElementSize } from '@vueuse/core';
+
+const wrapperRef$ = ref<HTMLElement>();
+const { width, height } = useElementSize(wrapperRef$);
+</script>
+
+<template>
+  <div ref="wrapperRef$" :class="$style['banner-container']">
+    <PatternMaterial :height="height" :width="width" />
+  </div>
+</template>
+
+<style module lang="scss" scoped>
+.banner {
+  &-container {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+}
+</style>
