719946778d58a7f6031aa0a61cbf3a8a08d3e6f2
aries0d0f <aries0d0f@gmail.com>
2023-04-04 16:59:09 +0800

    feat(route): Define route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/cfp',
+      component: () => import('../pages/CFP.vue')
+    },
+    {
+      path: '/',
+      redirect: '/cfp'
+    }
+  ]
 });
 
 export default router;
