aca2246ce6de9ccacce6e0ce7ab2aba7833f83fd
aries0d0f <aries0d0f@gmail.com>
2023-04-04 14:15:45 +0800

    build: Configure auto import component plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 import Vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -17,6 +18,12 @@ export default defineConfig({
         navigateFallback: 'index.html',
         navigateFallbackDenylist: [/.*\.(jpg|png|svg|json|js)$/]
       }
+    }),
+    Components({
+      dts: true,
+      deep: true,
+      dirs: ['src/components'],
+      include: [/\.vue$/, /\.vue\?vue/]
     })
   ],
   resolve: {
